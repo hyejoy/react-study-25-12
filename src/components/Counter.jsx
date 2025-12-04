89dd8e7e51f54edd8a55e0623f0038fe258b40d3
hyejoy <devchoi97@gmail.com>
2025-12-04 22:58:34 +0900

    docs: 함수 선언식과 함수 표현식의 차이 학습 및 코드 예시 추가

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+// 함수 표현식 vs 함수 선언식
+
+// 일반적으로 사용
+export default function Counter() {
+  // 상태 , 로직이 포함된경우
+  return <button>Cuonter</button>;
+}
+
+// UI만 담당하는 컴포넌트
+// export const Counter = () => <button>Counter</button>;
+// export default Counter;
