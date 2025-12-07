d0d922a013794d5bc1e853b4e11d682ccc2b3ee6
hyejoy <devchoi97@gmail.com>
2025-12-07 22:02:43 +0900

    feat : 삼항연산자·논리연산자 학습 및 CourseItem 컴포넌트 적용 - HeartIconBtn 삼항연산자 활용하여 동적 아이콘 렌더링 구현 - LinkIconBtn에서 논리연산자 && 를 이용한 단락 평가 기반 조건부 렌더링   적용 - 코드 간결화 및 가독성 향상

--- src/components/course/CourseItem.jsx
+++ src/components/course/CourseItem.jsx
@@ -1,15 +1,43 @@
-/* eslint-disable react/prop-types */
-export default function CourseItem({title , description, thumbnail}) {
+function HeartIconBtn({ isFavorite }) {
+  return (
+    <button className="btn">
+      {
+        <img
+          className="btn__img"
+          src={`/img/heart-${isFavorite ? "fill-" : ""}icon.svg`}
+        />
+      }
+    </button>
+  );
+}
+
+function LinkIconBtn({ link }) {
+  return (
+    link && (
+      <a className="btn" href={link} target="_blank" rel="noreferrer">
+        <img className="btn__img" src="/img/link-icon.svg" alt="" />
+      </a>
+    )
+  );
+}
+
+export default function CourseItem({
+  title,
+  description,
+  thumbnail,
+  isFavorite,
+  link,
+}) {
   return (
     <>
       <article className="course">
         <img className="course__img" src={thumbnail} alt="" />
         <div className="course__body">
-          <div className="course__title">
-            {title}
-          </div>
-          <div className="course__description">
-            {description}
+          <div className="course__title">{title}</div>
+          <div className="course__description">{description}</div>
+          <div className="course__icons">
+            {<HeartIconBtn isFavorite={isFavorite} />}
+            {<LinkIconBtn link={link} />}
           </div>
         </div>
       </article>
