25cf78f395f1c80f6c77a6176d29224c43714135
hyejoy <devchoi97@gmail.com>
2025-12-07 22:33:36 +0900

    feat: 리스트 렌더링 및 key 처리 규칙 적용 - map을 활용한 리스트 렌더링 구현 - Fragment로 여러 JSX 반환 처리 - key는 props 스프레드에 포함하지 않고 JSX에 직접 지정(React 경고 방지) - UUID로 고유 key 생성

--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -1,50 +1,20 @@
+import { Fragment } from "react";
 import Card from "../Card";
 import CourseItem from "./CourseItem";
-export default function CourseListCard() {
-  const courseList = [
-    {
-      title: "입문자를 위한, HTML&CSS 웹 개발 입문",
-      description: "웹 개발에 필요한 기본 지식을 배웁니다.",
-      thumbnail: "/img/htmlcss.png",
-      isFavorite: true,
-    },
-    {
-      title: "입문자를 위한, ES6+ 최신 자바스크립트 입문",
-      description: "쉽고! 알찬! 내용을 준비했습니다.",
-      thumbnail: "/img/js.png",
-      isFavorite: false,
-      link: "https://inf.run/Kpnd",
-    },
-    {
-      title: "포트폴리오 사이트 만들고 배포까지!",
-      description: "포트폴리오 사이트를 만들고 배포해 보세요.",
-      thumbnail: "/img/portfolio.png",
-      isFavorite: true,
-      link: "https://inf.run/YkAN",
-    },
-  ];
+import { v4 as uuidv4 } from "uuid";
 
+export default function CourseListCard({ title, courseList }) {
+  const lastIndex = courseList.length - 1;
   return (
-    <main>
-      <Card title="강의 목록">
-        {/* Card childern */}
-        {courseList.map((course) => (
-          <CourseItem
-            key={course.title}
-            title={course.title}
-            description={course.description}
-            thumbnail={course.thumbnail}
-            isFavorite={course.isFavorite}
-            link={course.link}
-          />
-        ))}
-      </Card>
-      {/* <div className="card">
-        <div className="card__header">강의 목록</div>
-        <div className="card__body">
-          <div className="courses"></div>
-        </div>
-      </div> */}
-    </main>
+    <Card title={title}>
+      {/* Card childern */}
+      {/* 여러개의 노드를 렌더링 하고 싶을때는 프레그먼트를 사용한다. */}
+      {courseList.map((course, index) => (
+        <Fragment key={uuidv4()}>
+          <CourseItem {...course} />
+          {index !== lastIndex && <hr className="divider" />}
+        </Fragment>
+      ))}
+    </Card>
   );
 }
