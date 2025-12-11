5295cbbd00d77c958a3ed6b79622f33f2f3d44b3
hyejoy <devchoi97@gmail.com>
2025-12-11 13:28:15 +0900

    feat : useState를 활용한 컴포넌트 상태 관리 학습 - useState로 컴포넌트별 독립적인 상태 생성 - 상태 변경 함수 (set)네이밍 규칙과 사용법 적용 - Counter 컴포넌트에서 부모 콜백(handlerTotal) 호출 흐름 구현 - 컴포넌트 인스턴스마다 상태가 개별적으로 관리됨을 확인

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,17 @@
 // 함수 표현식 vs 함수 선언식
 
+import { useState } from "react";
+
 // 일반적으로 사용
-export default function Counter() {
+export default function Counter({ handleTotal }) {
+  const [counter, setCounter] = useState(0);
+
+  const handleCounter = () => {
+    setCounter((prev) => prev + 1);
+    handleTotal();
+  };
   // 상태 , 로직이 포함된경우
-  return <button>Cuonter</button>;
+  return <button onClick={handleCounter}>counter : {counter}</button>;
 }
 
 // UI만 담당하는 컴포넌트
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Counter from "./Counter";
 
 export default function Main() {
+  const [total, setTotal] = useState(0);
+  const handleTotal = () => {
+    setTotal((prev) => prev + 1);
+  };
+
   return (
     <main>
-      <Counter />
+      <p>total Count : {total} </p>
+      <Counter handleTotal={handleTotal} />
+      <br />
+      <Counter handleTotal={handleTotal} />
     </main>
   );
 }
