7c5c8fc39b8a542b1bc6535da9a0e40a404555a8
hyejoy <devchoi97@gmail.com>
2025-12-20 10:50:15 +0900

    feat: React Context API로 다크모드 전역 상태 관리 구현 - CreateContext를 사용해 DarkModeContext 생성 - 최상위 컴포넌트에서 darkMode 상태와 토글 함수를 Provider로 전달 - Header/Main/Footer에서 props 없이 useContext로 상태 접근 - props drilling 문제를 Context API로 해결

---
+++ src/AppTeme.jsx
@@ -0,0 +1,25 @@
+import "./AppTheme.css";
+import HeaderTheme from "./components/theme/Header.jsx";
+import MainTheme from "./components/theme/Main.jsx";
+import FooterTheme from "./components/theme/Footer.jsx";
+import { DarkModeContext } from "./context/DarkModeContext.jsx";
+import { useState } from "react";
+function AppTheme() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode((darkMode) => !darkMode);
+  };
+
+  return (
+    <>
+      <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+        <HeaderTheme />
+        <MainTheme />
+        <FooterTheme />
+      </DarkModeContext.Provider>
+    </>
+  );
+}
+
+export default AppTheme;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./AppTodoReducer";
+import App from "./AppTeme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
