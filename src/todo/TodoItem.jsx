a66227aba09e2898e82a80dc36ad43553c4fc621
hyejoy <devchoi97@gmail.com>
2025-12-23 11:05:33 +0900

    feat: TodoItem에서 useEffect 라이프사이클 동작 확인 - 컴포넌트 마운트 시점에 effect 실행 - cleanup 함수를 통해 언마운트 타이밍 확인 목적

--- src/todo/TodoItem.jsx
+++ src/todo/TodoItem.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useTodoDispatch } from "../context/TodoContext";
 
 export default function TodoItem({ item }) {
@@ -12,6 +13,15 @@ export default function TodoItem({ item }) {
   const onDeleteTodo = (deleteId) => {
     dispatch({ type: "delete", deleteId });
   };
+
+  useEffect(() => {
+    console.log("컴포넌트가 마운트 되었습니다");
+
+    // 컴포넌트가 언마운트 될때 실행 (함수 리턴사용)
+    return () => {
+      console.log("컴포넌트가 언마운트 될 예정입니다.");
+    };
+  }, []);
 
   return (
     <label>
